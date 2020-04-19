5f4e1d51f4c99b711ca3504aaf40d14325606581
Danny Gale <danny.gale@gale-labs.com>
2020-04-18 17:02:58 -0700

    finish StagedAgent, fix inheritance for MPAgent, add the wait_step methods

--- crowd/agent.py
+++ crowd/agent.py
@@ -1,5 +1,8 @@
 
+import multiprocessing as mp
 from collections import OrderedDict
+from threading import Thread
+from queue import Queue
 
 class Agent:
     def __init__(self, uid:int, *args, **kwargs):
@@ -15,6 +18,10 @@ class Agent:
     def step(self, context):
         pass
 
+    def wait_step(self):
+        return
+
+
 class StagedAgent(Agent):
     """ An Agent that supports multiple stages per step
     stages are traversed in order
@@ -25,6 +32,7 @@ class StagedAgent(Agent):
             if not hasattr(self, method_name):
                 raise ValueError(f"{method_name} does not exist in {self.__class__.__name__} for stage {stage}")
         self.current_stage = self.stages.keys()[0]
+        self.current_stage_num = 0
 
         super().__init__(uid, *args, **kwargs)
 
@@ -33,10 +41,15 @@ class StagedAgent(Agent):
             self.current_stage = self.stages.keys()[0]
 
         # TODO: step through stages and call functions
+        getattr(self, self.stages[self.current_stage])(context)
+        self.current_stage = self.next_stage()
 
+    def next_stage(self):
+        self.current_stage_num += 1
+        if self.current_stage_num >= len(self.stages): 
+            self.current_stage_num = 0
+        return list(self.stages)[self.current_stage_num]
 
-from threading import Thread
-from queue import Queue
 class ThreadedAgent(Agent):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -60,9 +73,8 @@ class ThreadedAgent(Agent):
 
 
 
-import multiprocessing as mp
 
-class MPAgent:
+class MPAgent(Agent):
     def __init__(self, agent, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.subproc = mp.Process(target=self._thread)
@@ -77,11 +89,10 @@ class MPAgent:
             try:
                 ctx = self.queue.get(timeout = 1)
             except Empty:
-                # TODO: check if killed
                 if self.die.is_set():
                     return
             else:
-                self.agent.step(ctx)
+                self.step(ctx)
                 self.queue.task_done()
 
     def _step(self, context = {}):
@@ -89,4 +100,7 @@ class MPAgent:
 
     def kill(self):
         self.die.set()
+
+    def wait_step(self):
+        self.queue.join()
 
