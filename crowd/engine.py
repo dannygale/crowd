abfeeb5dc3dcd7e442cda89d87ae62507d42601b
Danny Gale <danny.gale@gale-labs.com>
2020-04-18 17:01:23 -0700

    update inheritance model, split to agent context and global context

--- crowd/engine.py
+++ crowd/engine.py
@@ -1,18 +1,19 @@
 from abc import ABC, abstractmethod
+import multiprocessing as mp
 
 from .model import Model
 from .agent import Agent, MPAgent
 
 class Engine(ABC):
-
     @abstractmethod
-    def run(self):
+    def run(self, model:Model, steps:int = 10):
         raise NotImplementedError(f"Must implement {self.__class__.__name__}.run")
 
-class BasicEngine(Engine):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    @abstractmethod
+    def step(self):
+        raise NotImplementedError(f"Must implement {self.__class__.__name__}.step")
 
+class BasicEngine(Engine):
     def run(self, model:Model, steps:int = 10):
         #print(type(model), model, type(steps), steps)
         for step in range(steps):
@@ -23,29 +24,21 @@ class BasicEngine(Engine):
                 return
 
     def step(self, model:Model):
-        #print(f'step {step}')
-        #print(f'context: {ctx}')
+        global_ctx = model.global_context()
         for agent in model.activate():
-            ctx = model.context(agent)
             #print(f'agent {agent.id}')
-            agent._step(ctx)
+            agent._step(global_ctx.update(model.agent_context(agent))))
             model.update(agent)
 
 
-import multiprocessing as mp
-from .agent import MPAgent
-
-class MPEngine(Engine):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._subproc = mp.Process(target = self._thread)
-        self._queue = mp.Queue()
-
+class MPEngine(BasicEngine):
     def run(self, model:Model, steps:int = 10):
-        self._subproc.start()
-
         super().run(model, steps)
-
         for agent in model.agents():
             agent.kill()
 
+    def step(self, *args, **kwargs):
+        super().step(*args, **kwargs)
+
+        for agent in model.agents():
+            agent.wait_step()
